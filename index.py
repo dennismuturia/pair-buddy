98491750bfb56e9c5aec91a0a34bc161ba705d93
dennismuturia <dmuturia2922@gmail.com>
2018-06-23 12:13:41 +0300

    Implemented the credentials to enable the user to interact with the Google sheets API

---
+++ index.py
@@ -0,0 +1,7 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#Using the credentilas to enable user to interact with the Google API
+scope = ['https://spreadsheets.google.com/feeds']
+credds = ServiceAccountCredentials.from_json_keyfile_name('Pair buddy-06f8df1913fe.json', scope)
+client = gspread.authorize(credds)
